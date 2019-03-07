cd7116dd6a9cade07e65738cde554931295d7222
Nilson Junior <nilsonsrjunior@gmail.com>
2019-03-07 20:49:04 -0300

    Exercicios 9 e 10

---
+++ Exercicio.Dez/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace Exercicio.Dez
+{
+    class Program
+    {
+        private readonly static FileSystemWatcher _watcher = new FileSystemWatcher();
+
+        static void Main(string[] args)
+        {
+            ListenToBankReturnFiles();
+        }
+
+        private static void ListenToBankReturnFiles()
+        {
+            _watcher.Path = GetConfigFolder();
+            _watcher.Filter = "*.RET";
+            _watcher.Created += OnCreated;
+            _watcher.EnableRaisingEvents = true;
+
+            new System.Threading.AutoResetEvent(false).WaitOne();
+        }
+
+        private static void OnCreated(object source, FileSystemEventArgs e)
+        {
+            Console.WriteLine($"O arquivo de retorno bancário {e.Name} foi recebido!");
+        }
+
+        public static string GetConfigFolder() => AppDomain.CurrentDomain.BaseDirectory;
+    }
+}
